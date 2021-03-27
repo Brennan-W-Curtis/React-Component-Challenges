d3af0ab4186e07a1896b6eec8754f65b26686bd5
A77soft <62488561+A77soft@users.noreply.github.com>
2021-03-27 14:18:21 -0400

    Create QuotesDatabase.js
    Created a folder to store a JavaScript file with JSON data for a Random Quote Machine application.

---
+++ randomQuoteMachine/QuotesDatabase.js
@@ -0,0 +1,35 @@
+const quotes = [
+    {
+        "quote": "Take some time to be lost. Give my heart something soft. Take some time with ourselves",
+        "author": "Rhye"
+    }, {
+        "quote": "Show me how you care. Tell me how you were loved before. Show me how you smile. Tell me why your hands are cold.",
+        "author": "Men I Trust"
+    }, {
+        "quote": "Say that you want to feel. Say that you want me near to you. Say that we will grow up. Together, and not apart.",
+        "author": "Blue Hawaii"
+    }, {
+        "quote": "The day that I met you I started dreaming. Now I write 'em down if I remember in the morning time.",
+        "author": "Faye Webster"
+    }, {
+        "quote": "So arrogant, baby little tween. Surfing in the mids. I am how I'm perceived.",
+        "author": "Okay Kaya"
+    }, {
+        "quote": "Saw you there with the kinked out hair and the killer stare. I wasn't prepared. Watching you sip on something sweet. Thinking of ways",
+        "author": "Pink $ock"
+    }, {
+        "quote": "Isn't it funny how it goes? The open-ended closed. Forcin' you, To enter as you go.",
+        "author": "Charlotte Day Wilson"
+    }, {
+       "quote": "And I just want to feel you're there. And I don’t want to have to share our love. I try, but I get overwhelmed.",
+       "author": "FKA Twigs" 
+    }, {
+       "quote": "The free fantasy. That carefree, that beginner's love. The sugar don't live up?",
+       "author": "Little Dragon"
+    }, {
+       "quote": "I can see the future, we'll be. Living it up inside. Making you feel alright, but I know. We got a little space between us.",
+       "author": "Homeshake"
+    }
+]
+
+export default quotes
