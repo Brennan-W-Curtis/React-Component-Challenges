b27137d7da5223b340c9ba757c95605b09e07608
Brennan Curtis <62488561+Brennan-W-Curtis@users.noreply.github.com>
2021-04-07 08:45:32 -0400

    Create initialState.js
    Created a JavaScript file with data to use when changing the App component's state.

---
+++ markdownPreviewer/initialState.js
@@ -0,0 +1,30 @@
+const initialState = `
+  This is a paragraph
+
+  **This is bolded text**
+
+  > This is a blockquote
+
+  # Heading
+  ## Heading
+  
+  - List Item 1
+  - List Item 2
+  - List Item 3
+  
+
+  [Visit my website](https://github.com/Brennan-W-Curtis)
+
+  This is an inline \`<div></div>\`
+
+  This is a block of code
+
+  \`\`\`
+    let a = 1;
+    let b = 2;
+    let c = a + b;
+  \`\`\`
+
+  ![freeCodeCamp](https://design-style-guide.freecodecamp.org/downloads/fcc_secondary_small.svg)
+`
+export default initialState;
