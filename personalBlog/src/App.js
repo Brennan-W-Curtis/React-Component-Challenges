a2206c08a524bfd286eb82cf8670a07f54f1182f
Brennan Curtis <62488561+Brennan-W-Curtis@users.noreply.github.com>
2021-09-16 13:00:38 -0400

    Create App.js
    Created a React Component to display the navigation bar and body content components for the Personal Blog application.

---
+++ personalBlog/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './Navbar';
+import Home from './Home';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
+
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/"><Home /></Route>
+            <Route path="/create"><Create /></Route>
+            <Route path="/blogs/:id"><BlogDetails /></Route>
+            <Route path="*"><NotFound /></Route> 
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
